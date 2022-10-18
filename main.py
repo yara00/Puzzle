989f42fcb1c49753cf9b4df7da7188c80fcdc810
yarahossamboo <yarahossam222@gmail.com>
2022-10-18 17:14:42 +0200

    Initial commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import State
 
 if __name__ == '__main__':
     s1 = State.State()
-    s1.set_state(123456780)
+    s1.set_state([1,2,3,4,5,6,7,8,0])
     print(s1.find_neighbours())
     res = str(123456789).zfill(10)
     print(res)
