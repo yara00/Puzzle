559abe0971992f883b034c75654e9a12aa57a6d4
yarahossamboo <yarahossam222@gmail.com>
2022-10-20 16:27:57 +0200

    BFS optimized final v

--- bfs.py
+++ bfs.py
@@ -23,11 +23,12 @@ class Bfs:
         startTime = time.time()
         # append init state to fringe queue
         currentStateNum = self.state.num
+        pastStateNum = currentStateNum
+        neighbour = 0
         self.fringe.put(currentStateNum)
         self.parentMap.update({self.state.num: -1})  # add root of parent map
 
         while not self.fringe.empty():
-            pastStateNum = currentStateNum  # keep track of last state
             currentStateNum = self.fringe.get()
             currentState = State.State(currentStateNum)
             self.explored.add(currentStateNum)  # append current state to explored set
@@ -37,10 +38,10 @@ class Bfs:
                 # compare goal state depth with past state's to get max depth
                 path = self.getPath(currentStateNum)
                 pastPath = self.getPath(pastStateNum)
-                self.maxDepth = max(len(path), self.maxDepth, len(pastPath))
+                self.maxDepth = max(len(path), len(pastPath))
                 path.reverse()
                 endTime = time.time()
-                return {"path": path, "cost": len(path) - 1, "max depth": self.maxDepth,
+                return {"path": path, "cost": len(path) - 1, "maxDepth": self.maxDepth - 1,
                         "expanded": len(self.explored) - 1, "time": endTime - startTime}
 
             # expand current state node
@@ -49,12 +50,12 @@ class Bfs:
                     # append neighbour to fringe queue
                     self.fringe.put(neighbour)
                     self.parentMap.update({neighbour: currentStateNum})
-
+            pastStateNum = neighbour  # keep track of last state
         # unsolvable case
-        return False
+        return {"path": [], "cost": 0, "maxDepth": 0, "expanded": 0, "time": 0}
 
 
 if __name__ == '__main__':
-    s = State.State(182043765)  # 125340678
+    s = State.State(12345678)  # 125340678
     bfs = Bfs(s)
     print(bfs.algorithm())
