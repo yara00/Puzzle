33868fc6b26b0c27d7151de5f11e9f5a1b02740b
yarahossamboo <yarahossam222@gmail.com>
2022-10-20 15:03:40 +0200

    input validation done

--- Controller.py
+++ Controller.py
@@ -5,9 +5,24 @@ import astar
 
 
 class Controller:
-    def solve(self, method, initialState):
-        state = State.State(initialState)
 
+    def input_validation(self, inputState):
+        if inputState < 12345678 or inputState > 876543210:
+            return False
+        if inputState < 100000000:
+            s = set('0' + str(inputState))
+        else:
+            s = set(str(inputState))
+
+        return s.issuperset("012345678")
+
+    def solve(self, method, initialState):
+        # verify input validity
+        if not self.input_validation(initialState):
+            return "Invalid Input"
+
+        state = State.State(initialState)
+        result = 0
         if method == "bfs":
             result = bfs.Bfs(state).algorithm()
         elif method == "dfs":
@@ -20,5 +35,5 @@ class Controller:
 
 if __name__ == '__main__':
     ctrl = Controller()
-    print(ctrl.solve("bfs", 125348670))
+    print(ctrl.solve("bfs", 876543889))
     print("ana t7t el control now")
