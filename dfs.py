6f780a6e99c58da69f3858bd0985f09ec123ae04
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2022-10-23 00:52:03 +0200

    Commented DFS

--- dfs.py
+++ dfs.py
@@ -8,12 +8,14 @@ class dfs:
     def __init__(self, initialState):
         self.state = initialState
         self.goalState = self.state.getGoalState()
-        self.frontier = deque()  # stack
-        self.explored = set()
+        self.frontier = deque()                         # stack
+        self.explored = set()                           # set of all expanded nodes
         self.parentMap = {}
         self.TempNeighbours = []
         self.maxDepth = 0
 
+# function take a state and backtrack in the parentMap with this state to get the path from the intial state to this searchState
+# return the path and the cost
     def get_path_and_cost(self, searchState):
         state = searchState
         path = []
@@ -23,43 +25,32 @@ class dfs:
 
         path.reverse()
         return path, len(path) - 1
-    '''
-    def get_Max_depth(self):
-        maxDepth = -10
-        print(self.parentMap.keys())
-        print(self.parentMap.values())
-        leafNodes = set(self.parentMap.keys()).difference(self.parentMap.values())
-        print(leafNodes)
 
-        for leaf in leafNodes:
-            x, depth = self.get_path_and_cost(leaf)
-            maxDepth = max(maxDepth, depth + 1)
-
-        return maxDepth
-    '''
     def algorithm(self):
         start = time.time()
+
+        # start with the intial state in the frontier and in the parent map
         self.frontier.append(self.state.num)
         self.parentMap.update({self.state.num: -1})
         depth = 0
+
         while self.frontier:
             currentNum = self.frontier.pop()
             currentState = State.State(currentNum)
             self.explored.add(currentNum)
-            #  print("Current state number is >>", currentNum)
             self.maxDepth = max(depth, self.maxDepth)
+
+            # solvable Case
             if currentState.isGoalState():
                 path, cost = self.get_path_and_cost(self.goalState)
                 self.maxDepth = max(len(path) - 1, self.maxDepth)
-                #  maxDepth = self.get_Max_depth()
                 end = time.time()
-               # print(self.parentMap)
                 return {"path": path, "cost": cost, "maxDepth": self.maxDepth, "expanded": len(self.explored) - 1,
                         "time": end - start}
-            flag = True
+
+            flag = True                     # to satisfy the depth
             for neighbour in currentState.find_neighbours():
                 if neighbour not in self.explored:
-                   # if neighbour not in self.frontier:
                     self.TempNeighbours.append(neighbour)
                     flag = False
 
@@ -68,12 +59,13 @@ class dfs:
             else:
                 depth = depth + 1
 
-            while self.TempNeighbours:
+            while self.TempNeighbours:              # reverse the states to maintain the same strategy
                 temp = self.TempNeighbours.pop()
                 self.frontier.append(temp)
                 self.parentMap.update({temp: currentNum})
 
-        # maxDepth = self.get_Max_depth()
+
+        # unsolvable Case
         end = time.time()
         return {"path": [], "cost": 0, "maxDepth": self.maxDepth, "expanded": len(self.explored), "time": end - start}
 
@@ -83,5 +75,4 @@ if __name__ == '__main__':
     s = State.State(182043765)  # 182043765        125340678       312045678
     search = dfs(s)
     print(search.algorithm())
-#  search.algorithm()
-#  search.get_Max_depth()
+
