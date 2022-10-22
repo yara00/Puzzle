7e12d164237c65607a845a6636a79c9d1b3f3151
yarahossamboo <yarahossam222@gmail.com>
2022-10-23 01:45:55 +0200

    DFS unit test

--- Unit Tests/TestDFS.py
+++ Unit Tests/TestDFS.py
@@ -2,15 +2,15 @@ import unittest
 import State
 import dfs
 
+
 class TestDFS(unittest.TestCase):
-
-
 
     def testUnsolvablePuzzle(self):
         state = State.State(812043765)
         result = dfs.dfs(state).algorithm()
         self.assertEqual(result["path"], [], "unsolvable puzzle path should be empty")
 
+    def testCost(self):
         state = State.State(125340678)
         result = dfs.dfs(state).algorithm()
         self.assertEqual(result["cost"], 3, "Cost = 3")
